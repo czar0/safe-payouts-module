0aa64fd6d64eeca7647c55fcedf31808b92467da
Cesare Valitutto <cesare.valitutto@gmail.com>
2023-11-20 14:25:26 +0000

    fix: zeroed mapping after deletion

--- src/SafePayoutsModule.sol
+++ src/SafePayoutsModule.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 /// @title An example implementation of a Safe module for recurring payouts
 /// @author Cesare Valitutto - @czar0
 /// @notice This contract implements methods to perform multiple payouts at once using funds available on the corresponding Safe account passed in input
-/// @dev As modules could represent a security threat for your Safe account, the following implementation measures are recommended for production-ready usage:
+/// @dev As modules could represent a security threat to your Safe account, the following implementation measures are recommended for production-ready usage:
 /// > Restricted access roles for operators should be defined and assigned by the owners
 /// > Avoid arbitrary values function parameters
 /// > Implement reentrancy guards
@@ -13,11 +13,11 @@ pragma solidity ^0.8.18;
 /// > Use Safe Guards
 /// For details on how to use this module with Safe, check the README
 contract SafePayoutsModule {
-    mapping(address => bool) private operators;
-    address[] private beneficiaries;
-    mapping(address => uint256) private payouts;
-    mapping(address => bool) private paid;
-    mapping(address => uint256) private indexes;
+    mapping(address => bool) public operators;
+    address[] public beneficiaries;
+    mapping(address => uint256) public payouts;
+    mapping(address => bool) public paid;
+    mapping(address => uint256) public indexes;
 
     modifier onlyOperator(address caller) {
         require(operators[caller], "no permissions");
@@ -53,12 +53,14 @@ contract SafePayoutsModule {
         beneficiaries[index] = beneficiaries[beneficiaries.length - 1];
         indexes[beneficiaries[index]] = index;
         beneficiaries.pop();
+        delete paid[beneficiary];
+        delete payouts[beneficiary];
     }
 
     /// Execute all the payouts (only if authorized)
     /// @param safe the address of the corresponding Safe account that enables this module
     /// @dev Limit allowance and define a timelock
-    function executePayouts(SafeModuleManager safe) public onlyOperator(msg.sender) {
+    function executePayouts(address safe) public onlyOperator(msg.sender) {
         for (uint256 i; i < beneficiaries.length; ++i) {
             address receiver = beneficiaries[i];
             if (!paid[receiver]) {
@@ -77,16 +79,16 @@ contract SafePayoutsModule {
         }
     }
 
-    function _transfer(SafeModuleManager safe, address token, address payable to, uint256 amount) private {
+    function _transfer(address safe, address token, address payable to, uint256 amount) private {
         if (token == address(0)) {
             require(
-                safe.execTransactionFromModule(to, amount, "", SafeModuleManager.Operation.Call),
+                ISafeModuleManager(safe).execTransactionFromModule(to, amount, "", ISafeModuleManager.Operation.Call),
                 "could not execute ether transfer"
             );
         } else {
-            bytes memory data = abi.encodeCall(SafeModuleManager.transfer, (to, amount));
+            bytes memory data = abi.encodeCall(ISafeModuleManager.transfer, (to, amount));
             require(
-                safe.execTransactionFromModule(token, 0, data, SafeModuleManager.Operation.Call),
+                ISafeModuleManager(safe).execTransactionFromModule(token, 0, data, ISafeModuleManager.Operation.Call),
                 "could not execute token transfer"
             );
         }
@@ -96,14 +98,14 @@ contract SafePayoutsModule {
 /// @title Interface for Safe Module Manager
 /// @author Cesare Valitutto - @czar0
 /// @notice The interface defines methods to perform transfers and execute call/delegatecall transactions on the Account via the Module Manager
-/// @dev Using interfaces reduces the amount of code imported by the main contract and, conseguently, the gas fees during the deployment phase
+/// @dev Using interfaces reduces the amount of code imported by the main contract and, consequently, the gas fees during the deployment phase
 /// However, interfaces can expose to incompatibility issues or errors in case the original contracts they refer to changed
-/// Before deploying the contract, it is recommended to verify the correctness of all methods and parameters or, alternatively, to refactor the code to use imports rather than interfaces
+/// Before deploying the contract, it is recommended to verify the correctness of all methods and parameters or to refactor the code to use imports rather than interfaces
 /// The version of the original contract is listed below:
 ///
 /// function execTransactionFromModule > ModuleManager.sol - https://github.com/safe-global/safe-contracts/blob/v1.3.0/contracts/base/ModuleManager.sol
 /// enum Operation > Enum.sol - https://github.com/safe-global/safe-contracts/blob/v1.3.0/contracts/common/Enum.sol
-interface SafeModuleManager {
+interface ISafeModuleManager {
     enum Operation {
         Call,
         DelegateCall
